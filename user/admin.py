fd8b01f4017b70493d24cf4afbaca06232dae6ba
mankic <miic0803@gmail.com>
2022-06-17 14:56:50 +0900

    수정 - user앱에 hobby 모델 추가

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, UserManager, UserProfile
+from .models import User, UserManager, UserProfile, Hobby
 
 # Register your models here.
 admin.site.register(User)
 # admin.site.register(UserManager)
 admin.site.register(UserProfile)
+admin.site.register(Hobby)
