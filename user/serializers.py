4b5441fa1e768558be0c1d047bba27a1c047c989
mankic <miic0803@gmail.com>
2022-06-21 16:30:53 +0900

    수정 - blog serializers 파일로 분리

--- user/serializers.py
+++ user/serializers.py
@@ -7,6 +7,9 @@ from user.models import Hobby as HobbyModel
 from blog.models import Article as ArticleModel
 from blog.models import Comment as CommentModel
 
+from blog.serializers import UserArticleSerializer
+
+
 class HobbySerializer(serializers.ModelSerializer):
     same_hobby_users = serializers.SerializerMethodField()
     def get_same_hobby_users(self,obj):
@@ -18,12 +21,15 @@ class HobbySerializer(serializers.ModelSerializer):
 
         # return user_list
 
+        user = self.context["request"].user
+
         # for 축약
-        return [user_profile.user.username for user_profile in obj.userprofile_set.all()]
+        return [user_profile.user.username for user_profile in obj.userprofile_set.exclude(user=user)]
 
     class Meta:
         model = HobbyModel
         fields = ["name","same_hobby_users"]
+
 
 class UserProfileSerializer(serializers.ModelSerializer):
     # 매니투매니 관계이므로 쿼리셋으로 return  # input data 쿼리셋이면 many=True
@@ -32,21 +38,13 @@ class UserProfileSerializer(serializers.ModelSerializer):
         model = UserProfileModel
         fields = ["age","birthday","hobby"]
 
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CommentModel
-        fields = ["content"]
 
-class UserArticleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ArticleModel
-        fields = ["category","title","content"]
 
 class UserSerializer(serializers.ModelSerializer):
     userprofile = UserProfileSerializer()   # 원투원관계의 경우 object로 return
     articles = UserArticleSerializer(many=True, source='article_set')
-    comments = CommentSerializer(many=True, source='comment_set')
+    
     class Meta:
         model = UserModel
         # fields = '__all__'  # 모든필드 가져온다
-        fields = ["username","email","join_date","userprofile","articles","comments"]
+        fields = ["username","email","join_date","userprofile","articles"]
