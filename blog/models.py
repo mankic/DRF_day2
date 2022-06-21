78c8d5a1f599523495a30df9fdc1bf387572ca24
mankic <miic0803@gmail.com>
2022-06-21 16:20:42 +0900

    수정 - Article 모델 노출 시작 종료 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,8 @@ class Article(models.Model):
     author = models.ForeignKey(User, verbose_name='작성자', on_delete=models.SET_NULL, null=True)
     title = models.CharField('제목', max_length=20)
     content = models.TextField('내용')
+    start_view = models.DateField('노출시작', null=True)
+    end_view = models.DateField('노출종료', null=True)
     
     def __str__(self):
         return self.title
